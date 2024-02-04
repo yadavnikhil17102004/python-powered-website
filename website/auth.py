55d33e6c7fa7a6b163e0bf00eb277d4e2f4fc330
nick <yadavnikhil17102004@gmail.com>
2024-02-04 21:56:39 +0530

    alert system added for sign up

--- website/auth.py
+++ website/auth.py
@@ -1,8 +1,8 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET',  'POST'])
 def  login():
     return render_template("login.html")
 
@@ -10,7 +10,21 @@ def  login():
 def  logout():
     return '<p>logout</p>'
 
-@auth.route('/sign_up')
+@auth.route('/sign_up', methods=['GET',  'POST'])
 def  sign_up():
-    # return '<p>sign up</p>'
+    if  request.method == "GET":
+        pass
+    elif request.method == "POST":
+        #do something with the data
+        email = request.form.get('email')
+        firstname = request.form.get('firstname')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if password1 != password2:
+            flash("Passwords Doesn't mach !!",category='error')
+        else :
+            #add use data to database
+            flash("Account successfull created !!",category='success')
+        
     return render_template("/sign_up.html")
