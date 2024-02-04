f4be6fc40fb184b216f8ba7e22a5239518e2b451
nick <yadavnikhil17102004@gmail.com>
2024-02-04 15:10:14 +0530

    Sign up page added

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
 def  login():
-    return '<p>login</p>'
+    return render_template("login.html")
 
 @auth.route('/logout')
 def  logout():
@@ -12,4 +12,5 @@ def  logout():
 
 @auth.route('/sign_up')
 def  sign_up():
-    return '<p>sign_up</p>'
+    # return '<p>sign up</p>'
+    return render_template("/sign_up.html")
