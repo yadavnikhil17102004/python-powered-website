00d3f9864b99b79ef588b5d8263e8a6ed60acd67
nick <yadavnikhil17102004@gmail.com>
2024-02-06 22:39:14 +0530

    logout and redirecting config

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash
 from .import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -14,17 +15,20 @@ def  login():
       if user:
           if (user.password, password):
               flash('Logged in successfully!', category='success')
+              login_user(user, remember=True)
               return redirect(url_for('views.home'))
           else:
               flash('Incorrect password, try again.', category='error')
       else:
           flash('Email does not exist.', category='error')
 
-    return render_template("login.html")
+    return render_template("login.html", user= current_user)
 
 @auth.route('/logout')
+@login_required
 def  logout():
-    return '<p>logout</p>'
+    logout_user()
+    return redirect(url_for( "auth.login"))
 
 @auth.route('/sign_up', methods=['GET',  'POST'])
 def  sign_up():
@@ -50,7 +54,8 @@ def  sign_up():
 
             #add use data to database
             flash("Account successfull created !!",category='success')
+            login_user(user, remember=True)
 
             return redirect(url_for('views.home'))
         
-    return render_template("/sign_up.html")
+    return render_template("/sign_up.html", user= current_user)
